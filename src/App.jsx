2b11d7d18d1fc66e61cbf27d9b8a7001d38d3c88
daxsum <kaldax250@gmail.com>
2023-10-16 14:16:09 +0300

    feat:navbar finished

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,14 @@ function App() {
   return (
     <>
       <div className="fixed top-0 w-screen h-screen bg-cover bg-[url('/home.gif')]">
-        <div className=" bg-[#1F3655] rounded w-[84.875rem] h-[7.4375rem] my-[3.1rem] mx-[4.1rem] flex justify-center items-center">
+        <div className=" bg-[#1F3655] rw-64 flex-shrink-0 rounded-lg  w-[84.875rem] h-[7.4375rem] my-[3.1rem] mx-[4.1rem] flex justify-between p-[1.56rem] items-center">
           <img src="/logo.png"></img>
-          <Dropdown animation="fade" color="red" />
+          <div className="flex justify-center items-center">
+            <Dropdown animation="fade" color="red" />
+            <button className="flex w-48 h-16 flex-col justify-center items-center rounded text-white border-2 border-zinc-50  text-center text-base not-italic font-bold">
+              LIVE CHAT
+            </button>
+          </div>
         </div>
       </div>
     </>
